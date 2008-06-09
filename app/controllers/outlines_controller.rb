cfa67e612491d93a7312252ef005c2c1c20ff495
wuts <wuts@sstp.cn>
2008-06-09 11:23:37 +0800

    edit outlines controller

--- app/controllers/outlines_controller.rb
+++ app/controllers/outlines_controller.rb
@@ -8,7 +8,7 @@ class OutlinesController < ApplicationController
                            :theme_advanced_resizing => true,
                            :theme_advanced_resize_horizontal => false,
                            :paste_auto_cleanup_on_paste => true,
-                           :theme_advanced_buttons1 => %w{fontsizeselect bold italic underline strikethrough separator justifyleft justifycenter justifyright indent outdent separator bullist numlist  separator undo redo},
+                           :theme_advanced_buttons1 => %w{fontsizeselect bold italic underline strikethrough separator justifyleft justifycenter justifyright indent outdent separator numlist  separator undo redo},
                            :theme_advanced_buttons2 => [],
                            :theme_advanced_buttons3 => [],
                            :plugins => %w{contextmenu paste}},
