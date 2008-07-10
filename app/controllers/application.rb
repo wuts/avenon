0e90300d8c4b2812ab608bb8f7238a072eee84ff
wuts <wuts@sstp.cn>
2008-07-10 23:35:22 +0800

    application controller

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -2,8 +2,8 @@
 # Likewise, all the methods added will be available for all controllers.
 
 class ApplicationController < ActionController::Base
-  helper :all # include all helpers, all the time
- #include AuthenticatedSystem
+ helper :all # include all helpers, all the time
+ include AuthenticatedSystem
  #include SharedHelper
  #include PreferencesHelper
 
